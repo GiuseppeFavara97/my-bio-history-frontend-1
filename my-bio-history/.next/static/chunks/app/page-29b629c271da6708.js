bf371ff501ca4ed000b6a6d87e647bd5cd012dda
GiuseppeFavara97 <giuseppe.favara1997@gmail.com>
2025-07-16 11:10:29 +0200

    chore: add sharp to ignored built dependencies in pnpm workspace configuration

---
+++ my-bio-history/.next/static/chunks/app/page-29b629c271da6708.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{1536:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9348,23))}},e=>{e.O(0,[348,265,538,358],()=>e(e.s=1536)),_N_E=e.O()}]);
